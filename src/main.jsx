c93205325ad95f4ea99712b1cf2e9a48d816e285
gwynbleiddrivia <yasinzme2@gmail.com>
2023-09-27 00:06:48 +0600

    coords source added to README.md

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import Home from './components/Home'
+import Main from './layout/Main'
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main> ,
+    children: [
+	{
+		path: "/",
+		element: <Home></Home>
+	}
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
